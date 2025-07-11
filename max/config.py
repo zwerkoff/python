80a2a9206029b294c934b2426cafa3ecbabe5cfb
zwerkoff <zwerkoff@gmail.com>
2025-07-11 09:29:03 +0300

    github

--- max/config.py
+++
@@ -1,4 +0,0 @@
-TOKEN='f9LHodD0cOJg9ytDU8Cgth6NQxgu2jeO_nf9x85l-u_gKJkadTLnIZpttO9D-Q7jpFxPCtGzigT-192wPxjv'
-SUPPORT=[4122223]
-WEBHOOK = 'https://tverregiongaz.ru/f9LHodD0cOJg9ytDU8Cgth6NQxgu2jeO'
-PORT=3002
